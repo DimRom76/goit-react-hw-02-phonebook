f1245f3a11b940c311c81226074b7b61cee08ba5
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-12-28 16:16:25 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteContact = this.deleteContact.bind(this);
+  }
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(el => el.id !== contactId),
+    }));
+  };
+
+  handleInput(event) {
+    this.setState({ filter: event.target.value });
+  }
+
+  onSubmit = values => {
+    const { contacts } = this.state;
+    if (
+      contacts.some(el => el.name.toLowerCase() === values.name.toLowerCase())
+    ) {
+      alert(`${values.name} is already in contacts`);
+      return;
+    }
+    values.id = uuidv4();
+    const newContacts = contacts.concat(values);
+    this.setState(prevState => {
+      return { ...prevState, contacts: newContacts };
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.onSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter handleInput={this.handleInput} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
